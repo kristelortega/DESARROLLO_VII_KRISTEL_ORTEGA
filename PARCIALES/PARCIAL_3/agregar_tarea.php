968b9579094b6b87059f5c1492b120e0d3a4d8b0
kristelortega <kristel.ortega@utp.ac.pa>
2024-10-24 20:11:27 -0500

    Modificación archivo agregar tarea

--- PARCIALES/PARCIAL_3/agregar_tarea.php
+++ PARCIALES/PARCIAL_3/agregar_tarea.php
@@ -7,5 +7,11 @@ if (!isset($_SESSION['username'])) {
     exit();
 }
 
+// Procesar el formulario de tareas
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $titulo = $_POST['titulo'];
+    $fecha_limite = $_POST['fecha_limite'];
+
+}
 
 ?>
